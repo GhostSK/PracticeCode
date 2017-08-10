4f1eb953222ac492a902549ed37bf0914c33115d
GhostSK <496774974@qq.com>
2017-08-10 16:55:27 +0800

    去掉多余注释

--- HealthKit/HealthKit/ViewController.m
+++ HealthKit/HealthKit/ViewController.m
@@ -39,53 +39,7 @@
         }
     }];
 }
-    
-    /*
-     //设置需要获取的权限仅设置了步数
-     HKObjectType *stepType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
-     HKObjectType *distanceType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning];
-     
-     NSSet *healthSet = [NSSet setWithObjects:stepType, distanceType, nil];
-     //从健康应用中获取数据
-     [self.healthStore requestAuthorizationToShareTypes:nil readTypes:healthSet completion:^(BOOL success, NSError * _Nullable error) {
-     if (success) {
-     //获取步数后我们调用获取步数的方法
-     [self readStepCount];
-     }
-     else
-     {
-     NSLog(@"获取步数权限失败");
-     }
-     }];
-     */
-    
-    /*
-     主要枚举：HKQuantityTypeIdentifierBodyMassIndex：体重指数
-     HKQuantityTypeIdentifierBodyFatPercentage：体脂率
-     HKQuantityTypeIdentifierHeight：身高
-     HKQuantityTypeIdentifierBodyMass：体重
-     HKQuantityTypeIdentifierLeanBodyMass：去脂体重
-     HKQuantityTypeIdentifierStepCount：行走步数
-     HKQuantityTypeIdentifierDistanceWalkingRunning：走跑距离
-     HKQuantityTypeIdentifierDistanceCycling：骑行距离
-     HKQuantityTypeIdentifierDistanceWheelchair：轮椅距离（喵喵喵？）
-     HKQuantityTypeIdentifierBasalEnergyBurned：基础能量消耗/静息能量消耗
-     HKQuantityTypeIdentifierActiveEnergyBurned：活动能量消耗
-     HKQuantityTypeIdentifierFlightsClimbed：爬升距离/已爬楼层
-     HKQuantityTypeIdentifierAppleExerciseTime：锻炼时间
-     HKQuantityTypeIdentifierPushCount：推动次数
-     HKQuantityTypeIdentifierDistanceSwimming：游泳距离  （这两项可能要手表数据，水果机不防水的吧
-     HKQuantityTypeIdentifierSwimmingStrokeCount：划水次数  
-     
-     HKQuantityTypeIdentifierHeartRate：心率
-     HKQuantityTypeIdentifierBodyTemperature：体温
-     HKQuantityTypeIdentifierBasalBodyTemperature 基础体温
-     HKQuantityTypeIdentifierBloodPressureSystolic 收缩血压/高压
-     HKQuantityTypeIdentifierBloodPressureDiastolic 舒张血压/低压
-     HKQuantityTypeIdentifierRespiratoryRate：呼吸频率
 
-     
-     */
 -(void)requestData{
     //设置你需要查询的采样信息
     //所有的查询类都是基于抽象类HKQuery，这里我们用查询步数来举例，步数对应的类是HKSample类，所以我们需要实例化一个HKSampleQuery对象
@@ -128,46 +82,6 @@
     [self.HKStore executeQuery:query];
 }
 
-
-    /*
--(void)readStepCount{
-    //查询拆样信息
-    HKSampleType *sampleType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
-    //NSSoryDescriptors用来告诉HealthStore排序方式
-    NSSortDescriptor *start = [NSSortDescriptor sortDescriptorWithKey:HKSampleSortIdentifierStartDate ascending:NO];
-    NSSortDescriptor *end = [NSSortDescriptor sortDescriptorWithKey:HKSampleSortIdentifierEndDate ascending:NO];
-
-     查询的基类是HKQuery,这是一个抽象类 能够实现每一种查询目标 这里我们需要查询的步数是一个
-     HKSample类所以对应的查询类就是HKSampleQuery
-     下面的limit参数传1表示查询最近一条数据，查询多条数据只要设置limit的参数值就可以了
-
-    HKSampleQuery *sampleQuery = [[HKSampleQuery alloc]initWithSampleType:sampleType predicate:[self predicateDorSamplesToday] limit:HKObjectQueryNoLimit sortDescriptors:@[start, end] resultsHandler:^(HKSampleQuery * _Nonnull query, NSArray<__kindof HKSample *> * _Nullable results, NSError * _Nullable error) {
-        int allstepCount = 0;
-        for (int i = 0; i <  results.count; i++) {
-            //把结果转换为字符串类型
-            HKQuantitySample *result = results[i];
-            HKQuantity *quantity = result.quantity;
-            NSMutableString *StepCount = (NSMutableString *)quantity;
-            NSString *StepStr = [NSString stringWithFormat:@"%@",StepCount];
-            //获取 51 count此类字符串前面的数字
-            NSString *str = [StepStr componentsSeparatedByString:@" "][0];
-            int StepNum = [str intValue];
-            NSLog(@"%d",StepNum);
-            //把一天中所有时间段中的步数加在一起
-            allstepCount += StepNum;
-        }
-        NSLog(@"今天总步数 === %d",allstepCount);
-        self.label.text = [NSString stringWithFormat:@"今日已行走%d步。",allstepCount];
-    }];
-    
-    //执行查询
-    [self.healthStore executeQuery:sampleQuery];
-    
-    
-    
-    
-}
-     */
 
 
 
